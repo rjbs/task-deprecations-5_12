b76b62fe74f4edb74897d628ef63ee8d1fc3275a
Ricardo Signes <rjbs@cpan.org>
2010-03-31 07:38:55 -0400

    first commit

---
+++ lib/Task/Deprecations/512.pm
@@ -0,0 +1,19 @@
+use 5.008;
+use strict;
+use warnings;
+package Task::Deprecations::512;
+# ABSTRACT: libraries deprecated from the core in 5.12.0
+
+=pkgroup Perl 5.12.0 Deprecations
+
+=pkg Class::ISA
+
+=pkg Pod::Plainer
+
+=pkg Shell
+
+=pkg Switch
+
+=cut
+
+1;
