62485fe3fbbba47759746cd7b58148069cde4ba3
Ricardo Signes <rjbs@cpan.org>
2010-03-31 16:32:37 -0400

    rename, version 1.000

--- lib/Task/Deprecations/5_12.pm
+++ lib/Task/Deprecations/5_12.pm
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-package Task::Deprecations::512;
+package Task::Deprecations::5_12;
 # ABSTRACT: libraries deprecated from the core in 5.12.0
 
 =pkgroup Perl 5.12.0 Deprecations
@@ -10,7 +10,7 @@ been marked as deprecated from the core in a way that will cause them to warn
 if used from the core distribution.  Libraries deprecated in 5.12 may no longer
 appear in future releases of perl 5.
 
-Installing this set of libraries (by installing Task::Deprecations::512 itself)
+Installing this set of libraries (by installing Task::Deprecations::5_12 itself)
 will stop the "will be removed from the Perl core distribution" warnings.
 
 =pkg Class::ISA
